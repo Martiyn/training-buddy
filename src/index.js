91227ef8cd66d8c4db74aa937fd70e7c9d0a0802
Martiyn <etigomartin4o0o@gmail.com>
2021-05-14 14:30:57 +0300

    Time to start over

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Trainer from './App.js';
-import 'bootstrap/dist/css/bootstrap.min.css'
 
 ReactDOM.render(
   <React.StrictMode>
