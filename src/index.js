d6e69bb668a05a48bca4ea5bc961a5fda8771058
Martiyn <etigomartin4o0o@gmail.com>
2021-06-14 13:31:17 +0300

    should work now

--- src/index.js
+++ src/index.js
@@ -3,11 +3,16 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import Trainer from './App.js'
 
+
+const TRAIN = [
+  {id: "exercise-0", name: "Pull ups (10)", completed: false},
+  {id: "exercise-1", name: "Bench press (5)", completed: false},
+  {id: "exercise-2", name: "Bicep curls (12)", completed: false}
+]
+
 ReactDOM.render(
   <React.StrictMode>
-    <Trainer />
+    <Trainer exercises={TRAIN} />
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-
